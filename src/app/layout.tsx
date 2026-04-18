3beff1d5c0009d6e083783c1fd26170a936e003a
Vedansh-Harish-Ail <ailvedansh@gmail.com>
2026-04-18 18:58:36 +0530

    fix: remove TEST DEPLOY overlay from layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -47,9 +47,7 @@ export default function RootLayout({
 
         {/* Vercel Analytics */}
         <Analytics />
-        <h1 style={{ color: "red", position: "fixed", top: 0 }}>
-          TEST DEPLOY
-        </h1>
+
       </body>
     </html>
   );
