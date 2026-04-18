f14dc3a0c2c1750e72c1629ad2ab19b628cb44b5
Vedansh-Harish-Ail <ailvedansh@gmail.com>
2026-04-18 12:53:33 +0530

    added GA + fixed layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -56,7 +56,22 @@ export default function RootLayout({
       </head>
 
       <body className="font-body min-h-full flex flex-col tracking-wide">
+
         {children}
+
+        {/* ✅ Google Analytics FIX */}
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-4F0T7KGBH2"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+      gtag('config', 'G-4F0T7KGBH2');
+    `}
+        </Script>
 
         {/* Vercel Analytics */}
         <Analytics />
